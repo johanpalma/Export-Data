9567ed4030c05056521c6f1acbf70dcac1589fd0
johan palma <jpalma@lean-tech.io>
2020-06-30 23:54:27 -0500

    export data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = require('./app');
 app.set('port', process.env.PORT || 4000);
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/submissions', { 
+    await mongoose.connect('mongodb+srv://userdb:userdb@cluster0.8w6zq.mongodb.net/googleDB?retryWrites=true&w=majority', { 
         useNewUrlParser: true,
         useUnifiedTopology: true })
           .then(() => {
